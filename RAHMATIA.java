d02214901d59522bbe1c211db3af312bc6eba7ef
Rahmatia21 <147253827+Rahmatia21@users.noreply.github.com>
2023-10-09 23:02:55 -0700

    Add files via upload

---
+++ RAHMATIA.java
@@ -0,0 +1,13 @@
+
+package tgs2_isc;
+
+public class RAHMATIA {
+    public static void main(String[] args) {
+    String nama = "rahmatia";
+    int umur = 21;
+    
+    
+    System.out.println("nama saya " + nama + ".");
+    System.out.println("Saya berusia " + umur + " tahun.");
+    } 
+}
